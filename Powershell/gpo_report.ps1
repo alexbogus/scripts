0ab62206d35eba737ab5b12ec7b0a29e477fa86f
ssppcc <alexbogus@gmail.com>
2015-04-27 00:25:46 +0200

    gpo-report.ps1
    Script para obtener un report HTML con todas las GPOs de Active
    Directory

---
+++ Powershell/gpo_report.ps1
@@ -0,0 +1,17 @@
+﻿import-module grouppolicy
+Get-GPOReport -All -ReportType Html -Path c:\scripts\gporeport.html
+
+#Set email variables
+$EmailFrom = "example@domain.tld"
+$EmailTo = "example@domain.tld"
+$Subject = "[ACTIVE DIRECTORY] GPO Report"
+$body = "Monthly GPO Report"
+$SMTPServer = "smtp.domain.tld"
+$report = "C:\scripts\gporeport.html"
+
+
+#Email the log files.
+Send-MailMessage -Subject $Subject -Body $body -SmtpServer $SMTPServer -Priority High -To $EmailTo -From $EmailFrom -Attachments $report
+
+# Remove logs
+Remove-Item $report
